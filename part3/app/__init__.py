5ae2d7322b4e3613b0c7fb49b6786a0bcd78e16a
Zairth <mano.gimenez@hotmail.com>
2025-03-12 16:11:42 +0100

    Adding the file admin.py where i will implement the admin API

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -5,6 +5,7 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
+from app.api.v1.admin import api as admin_ns
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
@@ -24,6 +25,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(admin_ns, path='/api/v1/admin')
 
     bcrypt.init_app(app)
 
